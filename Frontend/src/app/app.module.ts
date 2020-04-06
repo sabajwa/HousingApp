625aaf1bae351c78b8109d9936c09348daf24861
saeed <sabajwa@yahoo.com>
2020-04-06 15:14:47 -0400

    commit after changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-<<<<<<< HEAD
 import { HttpClientModule } from "@angular/common/http";
 import { PropertyCardComponent } from "./property/property-card/property-card.component";
 import { AppComponent } from "./app.component";
@@ -17,26 +16,5 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
-=======
-import { PropertyCardComponent } from "./property/property-card/property-card.component";
-import { AppComponent } from "./app.component";
-import { PropertyListComponent } from "./property/property-list/property-list.component";
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-@NgModule({
-   declarations: [
-      AppComponent,
-      PropertyCardComponent,
-      PropertyListComponent,
-      NavBarComponent
-   ],
-   imports: [
-	 BrowserModule
-	],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
->>>>>>> bdb86bf58baf3a8b16a4902af7d0594d25a98c9d
 })
 export class AppModule {}
